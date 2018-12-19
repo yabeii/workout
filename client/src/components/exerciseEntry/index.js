a80c2e27878638bfd16a27b830b6cfc7e0f530e8
pngo12 <pngo12@gmail.com>
2018-12-19 10:24:16 -0800

    basic flexbox/grid layout

--- client/src/components/exerciseEntry/index.js
+++ client/src/components/exerciseEntry/index.js
@@ -7,6 +7,8 @@ import LineChart from '../charts/Line';
 
 import { getEntriesGraph } from '../../helpers';
 
+import './exerciseEntry.css'
+
 class ExerciseEntry extends Component {
   state = {
     exercise: '',
@@ -29,7 +31,6 @@ class ExerciseEntry extends Component {
       [name]: value
     })
   }
-
   render() {
     let { exercise, set, rep, start } = this.state;
     let entries = getEntriesGraph({ exercise, set, rep }, this.props.workoutEntries);
@@ -37,35 +38,43 @@ class ExerciseEntry extends Component {
       this.setState({ start: true, exercise: Object.keys(this.props.exercises)[0] })
     }
     return (
-      <div>
-        <LineChart
-          title="Hello"
-          labels={entries.map(e => e.date)}
-          backgroundColor='rgba(75,192,192,1)'
-          borderColor='rgba(75,192,192,1)'
-          pointBorderColor='rgba(75,192,192,1)'
-          pointHoverBackgroundColor='rgba(75,192,192,1)'
-          pointHoverBorderColor='rgba(220,220,220,1)'
-          data={entries.map(({ entry }) => entry.weight)}
-        />
-        <ExerciseForm />
-        {
-          Object.keys(this.props.exercises).length > 0 && (
-            <React.Fragment>
-              <select value={exercise} name="exercise" onChange={this.changeHandler}>
-                {
-                  Object.keys(this.props.exercises).map((name, index) =>
-                    index === 0
-                      ? <option key={index} defaultChecked value={name}>{name}</option>
-                      : <option key={index} value={name}>{name}</option>
-                  )
-                }
-              </select>
-              <input type="number" name="set" value={set} placeholder="Set" onChange={this.changeHandler} />
-              <input type="number" name="rep" value={rep} placeholder="Rep" onChange={this.changeHandler} />
-            </React.Fragment>
-          )
-        }
+      <div className="container">
+        <div className="col-2"></div>
+        <div className="col-10">
+          <LineChart
+            title="Hello"
+            labels={entries.map(e => e.date)}
+            backgroundColor='rgba(75,192,192,1)'
+            borderColor='rgba(75,192,192,1)'
+            pointBorderColor='rgba(75,192,192,1)'
+            pointHoverBackgroundColor='rgba(75,192,192,1)'
+            pointHoverBorderColor='rgba(220,220,220,1)'
+            data={entries.map(({ entry }) => entry.weight)}
+          />
+
+          <div className="col-2">
+            <ExerciseForm />
+          </div>
+        </div>
+        <div className="col-2">
+          {
+            Object.keys(this.props.exercises).length > 0 && (
+              <React.Fragment>
+                <select value={exercise} name="exercise" onChange={this.changeHandler}>
+                  {
+                    Object.keys(this.props.exercises).map((name, index) =>
+                      index === 0
+                        ? <option key={index} defaultChecked value={name}>{name}</option>
+                        : <option key={index} value={name}>{name}</option>
+                    )
+                  }
+                </select>
+                <input type="number" name="set" value={set} placeholder="Set" onChange={this.changeHandler} />
+                <input type="number" name="rep" value={rep} placeholder="Rep" onChange={this.changeHandler} />
+              </React.Fragment>
+            )
+          }
+        </div>
       </div>
     );
   }
